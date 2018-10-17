cfb1b118bea2056879f2a5cce1ff06d641154dad
Owen-Krueger <owk914@gmail.com>
2018-10-17 16:24:32 -0500

    Original Commit

---
+++ PDFtoPPT/src/pdftoppt/PDFtoPPT.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pdftoppt;
+
+import java.io.*;
+import org.apache.poi.xslf.usermodel.XMLSlideShow;
+
+
+/**
+ *
+ * @author owk91
+ */
+public class PDFtoPPT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws FileNotFoundException, IOException{
+        XMLSlideShow ppt = new XMLSlideShow();
+        
+        //File file = new File("C://Users//owk91//Examples//example1.pptx");
+        //FileOutputStream out = new FileOutputStream(file);
+        //ppt.write(out);
+    }
+    
+}
