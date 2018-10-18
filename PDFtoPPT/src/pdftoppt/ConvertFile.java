e3067565eebbce84059c4b2c659b31416aaa4b27
Owen Krueger <okrueger@css.edu>
2018-10-18 12:54:34 -0500

    Added methods for converting images and adding to slideshow

--- PDFtoPPT/src/pdftoppt/ConvertFile.java
+++ PDFtoPPT/src/pdftoppt/ConvertFile.java
@@ -1,19 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pdftoppt;
 
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import javax.imageio.ImageIO;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.rendering.PDFRenderer;
+import org.apache.poi.sl.usermodel.PictureData;
+import org.apache.poi.xslf.usermodel.XMLSlideShow;
+import org.apache.poi.xslf.usermodel.XSLFPictureData;
+import org.apache.poi.xslf.usermodel.XSLFPictureShape;
+import org.apache.poi.xslf.usermodel.XSLFSlide;
 
 /**
  *
@@ -21,6 +24,10 @@ import org.apache.pdfbox.rendering.PDFRenderer;
  */
 public class ConvertFile {
     
+    /**
+     * Prompts user to select PDF file to open
+     * @return File pdf file
+     */
     public File openPDF(){
         JFrame frame = new JFrame();
         JFileChooser fileChooser = new JFileChooser();
@@ -37,6 +44,10 @@ public class ConvertFile {
         }
     }
     
+    /**
+     * Prompts user with save location for .pptx file
+     * @return File PowerPoint file
+     */
     public File savePPT(){
         JFrame frame = new JFrame();
         JFileChooser fileChooser = new JFileChooser();
@@ -53,10 +64,16 @@ public class ConvertFile {
         }
     }
     
+    /**
+     * Reads PDF file from user and creates ArrayList of images
+     * @param pdfFile File pdf from user prompt
+     * @return ArrayList<BufferedImage> image of each PDF page
+     * @throws IOException throws IO exception if fails to load pdfFile
+     */
     public ArrayList<BufferedImage> readPDF(File pdfFile) throws IOException{
         PDDocument pdfDoc = new PDDocument();
         pdfDoc = pdfDoc.load(pdfFile);
-        ArrayList images = new ArrayList<BufferedImage>();
+        ArrayList images = new ArrayList<>();
         
         PDFRenderer pdfRenderer = new PDFRenderer(pdfDoc);
         
@@ -66,11 +83,54 @@ public class ConvertFile {
         return images;
     }
     
-    public void convertImages(ArrayList<BufferedImage> images){
+    /**
+     * Converts ArrayList of images into Bytes to be used by XMLSlideshow
+     * @param images ArrayList<BufferedImage> array of images from PDF
+     * @return ArrayList<byte[]> array of converted images
+     * @throws IOException throws IO exception if fails to write image to ByteArray
+     */
+    public ArrayList<byte[]> convertImages(ArrayList<BufferedImage> images) throws IOException{
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ArrayList<byte[]> pictures = new ArrayList<>();
         
-        for(int i = 0; i < images.size(); i++){
-            
+        for (BufferedImage image : images) {
+            ImageIO.write(image, "jpg", baos);
+            byte[] imageByte = baos.toByteArray();
+            pictures.add(imageByte);
         }
+        
+        return pictures;
+    }
+    
+    /**
+     * 
+     * @param ppt
+     * @param pictures
+     * @return 
+     */
+    public XMLSlideShow addImages(XMLSlideShow ppt, ArrayList<byte[]> pictures){
+        PictureData pd;
+        XSLFSlide slide;
+        XSLFPictureShape ps;
+        
+        for(byte[] picture : pictures){
+            pd = ppt.addPicture(picture, XSLFPictureData.PictureType.JPEG);
+            slide = ppt.createSlide();
+            ps = slide.createPicture(pd);
+        }
+        
+        return ppt;
+    }
+    
+    /**
+     * 
+     * @param outFile
+     * @param ppt
+     * @throws FileNotFoundException
+     * @throws IOException 
+     */
+    public void finishFile(File outFile, XMLSlideShow ppt) throws FileNotFoundException, IOException{
+        FileOutputStream out = new FileOutputStream(outFile);
+        ppt.write(out);
     }
 }
