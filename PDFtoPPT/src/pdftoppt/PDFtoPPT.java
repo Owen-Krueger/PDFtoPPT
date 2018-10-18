fb195757b04890b71913768738b589e18e9feabe
Owen-Krueger <owk914@gmail.com>
2018-10-18 00:01:41 -0500

    Super gross... but it's working!

--- PDFtoPPT/src/pdftoppt/PDFtoPPT.java
+++ PDFtoPPT/src/pdftoppt/PDFtoPPT.java
@@ -7,12 +7,16 @@ package pdftoppt;
 
 import java.awt.image.BufferedImage;
 import java.io.*;
+import javax.imageio.ImageIO;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.rendering.PDFRenderer;
 import org.apache.poi.sl.usermodel.PictureData;
 import org.apache.poi.xslf.usermodel.XMLSlideShow;
+import org.apache.poi.xslf.usermodel.XSLFPictureData;
+import org.apache.poi.xslf.usermodel.XSLFPictureShape;
+import org.apache.poi.xslf.usermodel.XSLFSlide;
 
 
 /**
@@ -31,13 +35,18 @@ public class PDFtoPPT {
         PDFRenderer pdfRender;
         JFileChooser fileChooser = new JFileChooser();
         File pdfFile;
-        BufferedImage image;
+        BufferedImage image = null;
         
         if(fileChooser.showOpenDialog(parent) == JFileChooser.APPROVE_OPTION){
             pdfFile = fileChooser.getSelectedFile();
-            pdf.load(pdfFile);
+            System.out.println(pdfFile);
+            pdf = pdf.load(pdfFile);
+            System.out.println(pdf.getPage(0).toString());
             pdfRender = new PDFRenderer(pdf);
+            System.out.println(pdfRender.toString());
+            //for(int i = 0; i < pdf.getNumberOfPages(); i++)
             image = pdfRender.renderImage(0);
+            
             
         }
         
@@ -48,10 +57,19 @@ public class PDFtoPPT {
             }
             
             File file = new File(filename);
-            
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ImageIO.write( image, "jpg", baos );
+            byte[] imageByte = baos.toByteArray();
             //File file = fileChooser.getSelectedFile();
+            
+            PictureData idx = ppt.addPicture(imageByte, XSLFPictureData.PictureType.JPEG);
+      
+            XSLFSlide slide = ppt.createSlide();
+            
+      //creating a slide with given picture on it
+            XSLFPictureShape pic = slide.createPicture(idx);
+            
             FileOutputStream out = new FileOutputStream(file);
-            ppt.createSlide();
             
             //ppt.addPicture(image, PictureData.PictureType.EMF);
             ppt.write(out);            
